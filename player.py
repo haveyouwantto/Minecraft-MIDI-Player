119be30ed964b3729c5b7e29774789f90932c8c4
haveyouwantto <haveyouwantto@gmail.com>
2019-12-06 22:47:24 +0800

    add inst maps

--- player.py
+++ player.py
@@ -2,25 +2,15 @@ import asyncio
 import glob
 import json
 import os
-import sys
 import threading
 import time
+import sys
+
+import drum_set
+import instruments_map
 
 import mido
 import websockets
-
-
-sub = json.dumps({
-    "body": {
-        "eventName": "PlayerMessage"
-    },
-    "header": {
-        "requestId": "0ffae098-00ff-ffff-abbb-bbbbbbdf3344",
-        "messagePurpose": "subscribe",
-        "version": 1,
-        "messageType": "commandRequest"
-    }
-})
 
 
 def cmd(line):
@@ -41,8 +31,70 @@ def cmd(line):
     })
 
 
+sub = json.dumps({
+    "body": {
+        "eventName": "PlayerMessage"
+    },
+    "header": {
+        "requestId": "0ffae098-00ff-ffff-abbb-bbbbbbdf3344",
+        "messagePurpose": "subscribe",
+        "version": 1,
+        "messageType": "commandRequest"
+    }
+})
+
+helpmsg = {
+    '.test': '测试用命令    \u00a7c.test',
+    '.help': '提供帮助/命令列表    \u00a7c.help',
+    '.function': '运行在相应的功能文件中找到的命令    \u00a7c.function <function>',
+    '.midi': '播放一个mid文件    \u00a7c.midi <file>'
+}
+
+play = True
+
+
 def getChat(msg):
     return msg["body"]["properties"]["Message"]
+
+
+def info(msg):
+    return cmd("say \u00a7d" + str(msg))
+
+
+def drawKeyboard(key, start=0):
+    out = ""
+    i = start
+    while i < key:
+        if (i % 12 == 1 or i % 12 == 3 or i % 12 == 6 or i % 12 == 8 or i % 12 == 10):
+            out += "\u00a70\u258F"
+        else:
+            out += "\u00a7f\u258F"
+        i += 1
+    return out
+
+
+def midiDisplay(midimsg):
+    out = '/titleraw @s actionbar {"rawtext":[{"text":"\u00a70'
+    i = 0
+    out += drawKeyboard(midimsg.note)
+    out += "\u00a7c\u258F"
+    out += drawKeyboard(128, midimsg.note+1)
+    i += 1
+
+    return cmd(out+'"}]}')
+
+
+def play_note(midimsg, inst):
+    origin = midimsg.note - 66
+    instrument = instruments_map.inst_map[inst]
+    pitch = 2 ** ((origin+instrument[1]*12) / 12)
+    volume = midimsg.velocity / 128
+    return cmd("execute @a ~ ~ ~ playsound "+instrument[0]+" @s ^0 ^ ^ " + str(volume) + " " + str(pitch))
+
+def play_perc(midimsg):
+    instrument = drum_set.drum_set[midimsg.note]
+    volume = midimsg.velocity / 128
+    return cmd("execute @a ~ ~ ~ playsound "+instrument+" @s ^0 ^ ^ " + str(volume))
 
 
 def runmain(coroutine):
@@ -52,95 +104,117 @@ def runmain(coroutine):
         return e.value
 
 
-def info(msg):
-    return cmd("say \u00a7d" + str(msg))
-
-
-def drawKeyboard(e,s=0):
-    out=""
-    i=s
-    while i < e:
-        if (i % 12 == 1 or i % 12 == 3 or i % 12 == 6 or i % 12 == 8 or i % 12 == 10):
-            out += "\u00a70\u258F"
-        else:
-            out += "\u00a7f\u258F"
-        i+=1
-    return out
-
-def midiDisplay(midimsg):
-    out = '/titleraw @s actionbar {"rawtext":[{"text":"\u00a70'
-    i = 0
-    out += drawKeyboard(midimsg.note)
-    out += "\u00a7c\u258F"
-    out += drawKeyboard(128,midimsg.note+1)
-    i+=1
-    
-    return cmd(out+'"}]}')
-
-
-def play_note(midimsg):
-    origin = midimsg.note - 66
-    pitch = 2 ** (origin / 12)
-    volume = midimsg.velocity / 128
-    return cmd("execute @a ~ ~ ~ playsound note.harp @s ^0 ^ ^ " + str(volume) + " " + str(pitch))
-
-
 class midiplayer(threading.Thread):
-    def __init__(self, ws, mid):
+    def __init__(self, ws):
         threading.Thread.__init__(self)
         self.ws = ws
-        self.play = True
-        self.mid = mido.MidiFile(mid)
+        self.playing = False
+        self.mid = None
+        self.setName('Midi Player Thread')
 
     def run(self):
-        for msg in self.mid.play():
-            if msg.type == "note_on":
-                print(msg)
-                runmain(self.ws.send(play_note(msg)))
-                runmain(self.ws.send(midiDisplay(msg)))
-            if not self.play:
-                break
+        while True:
+            if self.playing:
+                inst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                try:
+                    for msg in self.mid.play():
+                        if msg.type == "note_on":
+                            if msg.channel != 9:
+                                runmain(self.ws.send(
+                                    play_note(msg, inst[msg.channel])))
+                            else:
+                                runmain(self.ws.send(play_perc(msg)))
+                        if msg.type == "program_change":
+                            inst[msg.channel] = msg.program
+                            print(inst)
+                        if not self.playing:
+                            break
+                except Exception as e:
+                    runmain(self.ws.send(info(e)))
+                    self.mid = None
+                    self.playing = False
+
+    def set_midi(self, mid):
+        self.mid = mido.MidiFile(mid)
+
+    def play(self):
+        self.playing = True
 
     def stop(self):
-        self.play = False
+        self.playing = False
+
+
+def setBlock(x, y, z, id, data=0):
+    return cmd("setblock " + str(x) + " " + str(y) + " " + str(z) + " " + str(id) + " " + str(data))
+
+
+def miidDisplay():
+    out = '/titleraw @s actionbar {"rawtext":[{"text":"'
+    for i in range(128):
+        out += "\u258F"
+    return out+'"}]}'
 
 
 async def hello(ws, path):
+
+    player = midiplayer(ws)
+    player.start()
+
     await ws.send(sub)
 
     sender = "外部"
 
+    log = open('log.txt', 'a')
+
     while True:
         data = await ws.recv()
         msg = json.loads(data)
-        print(data)
-        midiplayers = []
         if msg["header"]["messagePurpose"] == "event":
 
             if msg["body"]["eventName"] == "PlayerMessage" and msg["body"]["properties"]["Sender"] != sender:
 
                 raw = getChat(msg)
 
+                log.write(raw+"\n")
+
                 args = raw.split(" ")
-                if raw.startswith(".midi"):
+
+                if args[0] == ".test":
+                    await ws.send(info("Hello World"))
+
+                if args[0] == ".help":
+                    for i in helpmsg:
+                        await ws.send(info(i + " - " + helpmsg[i]))
+
+                if args[0] == ".quit":
+                    log.close()
+                    sys.exit()
+
+                if args[0] == ".midi":
                     try:
                         midils = glob.glob("midis/*.mid")
                         if args[1] == "-ls":
                             for i in range(len(midils)):
                                 await ws.send(info('[§c{0}§d] - {1}'.format(i, midils[i])))
                         elif args[1] == "-stop":
-                            for i in midiplayers:
-                                i.stop()
+                            player.stop()
+                            await ws.send(info("正在停止"))
                         else:
                             arg1 = int(args[1])
                             if arg1 < len(midils):
-                                await ws.send(info("正在加载 " + midils[arg1]))
-                                player = midiplayer(ws, midils[arg1])
-                                midiplayers.append(player)
-                                player.start()
+                                await ws.send(info("正在加载 " + midils[arg1] + "..."))
+                                player.stop()
+                                player.set_midi(midils[arg1])
+                                player.play()
+                                await ws.send(miidDisplay())
+                                """ 
+                                mid = mido.MidiFile(midils[arg1])
+                                for msg in mid.play():
+                                    if msg.type == "note_on":
+                                        print(msg)
+                                        await ws.send(play_note(msg)) """
                             else:
                                 await ws.send(info("文件不存在"))
-
                     except Exception as e:
                         await ws.send(info(str(e)))
 
@@ -148,7 +222,7 @@ async def hello(ws, path):
             pass
 
 
-start_server = websockets.serve(hello, "localhost", 19111)
+start_server = websockets.serve(hello, "0.0.0.0", 19111)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
